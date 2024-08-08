448ddcfddad175bb3c3162895d99900f8c79214d
frank <frankode254@gmail.com>
2024-08-08 13:03:52 -0700

    Adding new taks

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- Task: Create a table users with specified requirements
+-- This script creates a table named users with the following attributes:
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
+);
---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- Task: Create a table users with specified requirements
+-- This script creates a table named users with the following attributes:
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);
