ff44e56315eb1bf667f3fe0deb8dfae8c3475b34
codefox254 <91884653+codefox254@users.noreply.github.com>
2024-08-13 14:13:37 -0700

    Update 8-main.py

--- 0x01-NoSQL/8-main.py
+++ 0x01-NoSQL/8-main.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """ 8-main """
-from pymongo import MongoClient
-list_all = __import__('8-all').list_all
 
-if __name__ == "__main__":
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    school_collection = client.my_db.school
-    schools = list_all(school_collection)
-    for school in schools:
-        print("[{}] {}".format(school.get('_id'), school.get('name')))
+
+def list_all(mongo_collection):
+    """
+    Lists all documents in a collection.
+
+    :param mongo_collection: The pymongo collection object.
+    :return: A list of all documents in the collection. Returns an empty list if no documents are found.
+    """
+    return mongo_collection.find()
