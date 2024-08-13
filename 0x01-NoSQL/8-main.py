46247bb16e777a5f24063201041b4553b28236f0
codefox254 <91884653+codefox254@users.noreply.github.com>
2024-08-13 14:14:45 -0700

    Update 8-main.py

--- 0x01-NoSQL/8-main.py
+++ 0x01-NoSQL/8-main.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
-""" 8-main """
+"""
+List all documents in a collection using pymongo
+"""
+
+
+from pymongo import MongoClient
 
 
 def list_all(mongo_collection):
@@ -9,4 +14,4 @@ def list_all(mongo_collection):
     :param mongo_collection: The pymongo collection object.
     :return: A list of all documents in the collection. Returns an empty list if no documents are found.
     """
-    return mongo_collection.find()
+    return list(mongo_collection.find())
